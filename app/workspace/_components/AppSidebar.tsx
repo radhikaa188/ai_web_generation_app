db5c49c804f255827aace24a494efc39bf6981fe
Radhika Agarwal <radhikaag18l@gmail.com>
2026-02-03 14:38:49 +0530

    display projects list

--- app/workspace/_components/AppSidebar.tsx
+++ app/workspace/_components/AppSidebar.tsx
@@ -9,15 +9,28 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar"
 import Link from "next/link"
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Image from "next/image"
 import { UserDetailContext } from "@/context/UserDetailContext"
 import { Progress } from "@/components/ui/progress"
 import { UserButton } from "@clerk/nextjs"
+import axios from "axios"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export function AppSidebar() {
     const [projectList, setProjectList] = useState([])
     const {userDetail, setUserDetail} = useContext(UserDetailContext)
+    const [loading, setLoading] = useState(false)
+    useEffect(()=>{
+      GetProjects()
+    },[]) 
+    const GetProjects =async()=>{
+      setLoading(true)
+      const result = await axios.get('/api/get-all-projects')
+      console.log(result.data)
+      setProjectList(result.data)
+      setLoading(false)
+    }
     return (
     <Sidebar>
       <SidebarHeader className="p-5">
@@ -35,8 +48,19 @@ export function AppSidebar() {
       <SidebarContent className="p-2">
         <SidebarGroup>
             <SidebarGroupLabel>Projects</SidebarGroupLabel>
-            {projectList.length==0 && 
+            {!loading && projectList.length==0 && 
              <h2 className="text-sm px-2 text-gray-500">No Project Found</h2>}
+            <div>
+              { (!loading && projectList.length>0)? projectList.map((project:any, index)=>(
+                <Link href={`/playground/${project.projectId}?frameId=${project.frameId}`} key={index} className="my-2 hover:bg-secondary p-2 rounded-lg  cursor-pointer">
+                  <h2 className="line-clamp-1">{project?.chats[0].chatMessage[0]?.content}</h2>
+                </Link>
+              )):
+              [1,2,3,4,5].map((_, index)=>(
+                 <Skeleton className="w-full h-10 rounded-lg mt-2"/>
+              ))
+            }
+            </div>
         </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
